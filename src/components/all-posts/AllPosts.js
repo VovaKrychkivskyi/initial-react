55d38aa4a84e2ee03adfa51c3453493a4234a064
VovaKrychkivskyi <4Fa263izmnSurDnTa8fY>
2020-12-10 11:20:15 +0200

    add router

--- src/components/all-posts/AllPosts.js
+++ src/components/all-posts/AllPosts.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import {
+  Link,
+  withRouter
+} from "react-router-dom"
 
 class AllPosts extends Component {
   state = {posts: []}
@@ -12,12 +16,13 @@ class AllPosts extends Component {
 
   render() {
     let {posts} = this.state
+    let {match: {url}} = this.props
     return (
       <div>
         {
-          posts.map(value => <div key = {value.id}>
-            {value.title}
-          </div> )
+          posts.map(value => <div key={value.id}>
+            {value.title} - <Link to={`${url}/${value.id}`}>deteils</Link>
+          </div>)
         }
 
       </div>
@@ -25,4 +30,4 @@ class AllPosts extends Component {
   }
 }
 
-export default AllPosts;
+export default withRouter(AllPosts);
